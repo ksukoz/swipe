beefb994a4a82cd52f1601a41417c436538c8d99
ksukoz <seniurbano@gmail.com>
2018-12-08 17:08:35 +0200

    Create a ball component for testing animated module

---
+++ src/Ball.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export default class Ball extends Component {
+	render() {
+		return <View style={styles.ball} />;
+	}
+}
+
+const styles = StyleSheet.create({
+	ball: {
+		height: 60,
+		width: 60,
+		borderRadius: 30,
+		backgroundColor: 'black'
+	}
+});
