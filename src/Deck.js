1e69dc7af61903f6bdd0bc0bf3ea962f9c21eba6
ksukoz <seniurbano@gmail.com>
2018-12-08 21:07:34 +0200

    Implement animation to a single card

--- src/Deck.js
+++ src/Deck.js
@@ -19,16 +19,19 @@ export default class Deck extends Component {
 	}
 
 	renderCards() {
-		return this.props.data.map((item) => {
+		return this.props.data.map((item, i) => {
+			if (i === 0) {
+				return (
+					<Animated.View style={this.state.position.getLayout()} {...this.state.panResponder.panHandlers}>
+						{this.props.renderCard(item)}
+					</Animated.View>
+				);
+			}
 			return this.props.renderCard(item);
 		});
 	}
 
 	render() {
-		return (
-			<Animated.View style={this.state.position.getLayout()} {...this.state.panResponder.panHandlers}>
-				{this.renderCards()}
-			</Animated.View>
-		);
+		return <View>{this.renderCards()}</View>;
 	}
 }
