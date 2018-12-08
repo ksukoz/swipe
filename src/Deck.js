955759bf3711b993f2419cb23b44f55da34872c9
ksukoz <seniurbano@gmail.com>
2018-12-08 21:02:12 +0200

    Implement dragging cards

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 export default class Deck extends Component {
+	constructor(props) {
+		super(props);
+
+		const position = new Animated.ValueXY();
+
+		const panResponder = PanResponder.create({
+			onStartShouldSetPanResponder: () => true,
+			onPanResponderMove: (event, gesture) => {
+				position.setValue({ x: gesture.dx, y: gesture.dy });
+			},
+			onPanResponderRelease: () => {}
+		});
+
+		this.state = { panResponder, position };
+	}
+
 	renderCards() {
 		return this.props.data.map((item) => {
 			return this.props.renderCard(item);
@@ -9,6 +25,10 @@ export default class Deck extends Component {
 	}
 
 	render() {
-		return <View>{this.renderCards()}</View>;
+		return (
+			<Animated.View style={this.state.position.getLayout()} {...this.state.panResponder.panHandlers}>
+				{this.renderCards()}
+			</Animated.View>
+		);
 	}
 }
