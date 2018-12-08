c3e94470e06c8cc6638580c2100717862f28a61d
ksukoz <seniurbano@gmail.com>
2018-12-08 23:23:05 +0200

    Add reset position function after tap release

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default class Deck extends Component {
 	constructor(props) {
@@ -11,17 +13,26 @@ export default class Deck extends Component {
 			onStartShouldSetPanResponder: () => true,
 			onPanResponderMove: (event, gesture) => {
 				position.setValue({ x: gesture.dx, y: gesture.dy });
+				return true;
 			},
-			onPanResponderRelease: () => {}
+			onPanResponderRelease: () => {
+				this.resetPosition();
+			}
 		});
 
 		this.state = { panResponder, position };
 	}
 
+	resetPosition() {
+		Animated.spring(this.state.position, {
+			toValue: { x: 0, y: 0 }
+		}).start();
+	}
+
 	getCardStyle() {
 		const { position } = this.state;
 		const rotate = position.x.interpolate({
-			inputRange: [ -500, 0, 500 ],
+			inputRange: [ -SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5 ],
 			outputRange: [ '-120deg', '0deg', '120deg' ]
 		});
 
